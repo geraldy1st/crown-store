4717f9e7a2d16fc10d817046d5bc5ea417a5a894
geraldy1st <geraldy.leondas@gmail.com>
2022-07-25 13:55:26 +0200

    created button

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,24 @@
+/*
+There is 3 different type of btn
+default btn
+inverted btn
+google btn
+*/
+import "./button.styles.scss";
+
+const BUTTON_TYPE_CLASSES = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
