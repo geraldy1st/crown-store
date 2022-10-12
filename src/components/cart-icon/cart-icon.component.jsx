f18e11a9ce975891d17ca42662c5616f4b784b8f
geraldy1st <geraldy.leondas@gmail.com>
2022-10-12 16:22:08 +0200

    using reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,7 +6,9 @@ import { ShoppingIcon, CartIconContainer, ItemCount } from "./cart-icon.styles";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+  const toggleIsCartOpen = () => {
+    setIsCartOpen(!isCartOpen);
+  };
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
       <ShoppingIcon />
